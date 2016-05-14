1b255234b021ff20f0fee08d6b676b3e8543cb29
dratech <alfonsotech@nym.hush.com>
2016-05-14 12:19:50 -0700

    commit message

--- js/app.js
+++ js/app.js
@@ -11,15 +11,36 @@ $(document).ready(function() {
   })
 
 
-  	.mousedown(function() {
-  // play hadouken sound
+.mousedown(function() {
+  playHadouken();
   $('.ryu-ready').hide();
   $('.ryu-throwing').show();
-  $('.hadouken').show();
-  // animate hadouken to the right of the screen
+  $('.hadouken').finish().show()
+  .animate(
+    {'left': '1020px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).css('left', '520px');
+    }
+  );
 })
   .mouseup(function() {
   $('.ryu-throwing').hide();
   $('.ryu-ready').show();
 });
-});
+  $('.hadouken').show().animate(
+  {'left': '1020px'},
+  500,
+  function() {
+    $(this).hide();
+    $(this).css('left', '520px');
+  }
+);
+}); //document ready end
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
